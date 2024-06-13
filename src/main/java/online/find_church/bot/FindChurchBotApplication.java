1777a57050d5c444119b441474c6b9a440c7ddd1
whoiswho68rus@gmail.com <githubRovnKresRim4>
2024-06-14 00:05:29 +0400

    first commit

---
+++ src/main/java/online/find_church/bot/FindChurchBotApplication.java
@@ -0,0 +1,13 @@
+package online.find_church.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindChurchBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindChurchBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/find_church/bot/FindChurchBotApplicationTests.java
@@ -0,0 +1,13 @@
+package online.find_church.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindChurchBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
